9ff6071efe331717215711129f7a323e7230b44a
Thierry DECKER <mail@thierry-decker.com>
2018-01-25 06:22:21 +0100

    Updated project structure

---
+++ nfpy/nfcol/nfcol.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+def main():
+    """The entry point of the script
+
+    This function will act as an entry point for the script.
+
+    Args:
+        None
+
+    Returns:
+        None
+
+    Raises:
+        None
+
+    """
+    pass
+
+
+if __name__ == '__main__':
+    #
+    # Call the main() function
+    #
+    main()
