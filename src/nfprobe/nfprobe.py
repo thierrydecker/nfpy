e743f4d6c3c598a370bc40f90070db67acb3b01b
Thierry DECKER <mail@thierry-decker.com>
2018-02-04 19:47:29 +0100

    Updated nfprobe

--- src/nfprobe/nfprobe.py
+++ src/nfprobe/nfprobe.py
@@ -1,23 +1,32 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""This is the main module of the Netflow probe
+
+"""
+import signal
+import sys
+import time
+
+
+def nfprobe_stopper(signum, stack):
+    """Stops the Netflow probe
+
+    :param signum: the signal number received
+    :param stack: the stack when signal was received
+    """
+    print('NFProbe stopped')
+    sys.exit(0)
 
 
 def main():
-    """The entry point of the script
-
-    This function will act as an entry point for the script.
-
-    Args:
-        None
-
-    Returns:
-        None
-
-    Raises:
-        None
+    """Starts the Netflow probe
 
     """
-    pass
+    signal.signal(signal.SIGINT, nfprobe_stopper)
+    print('NFProbe started')
+    while True:
+        print('NFProbe is running... Press CTRL+C to stop it.')
+        time.sleep(1)
 
 
 if __name__ == '__main__':
