4d8578bff40870c4c08531fda46a2931dbdb7c26
Thierry DECKER <mail@thierry-decker.com>
2018-02-10 07:23:34 +0100

    Updated helpers.py

--- src/commons/helpers.py
+++ src/commons/helpers.py
@@ -9,21 +9,21 @@ import json
 
 
 def get_adapters_names():
-    '''Finds all adapters on the system
+    """Finds all adapters on the system
 
     :return: A list of the network adapters available on the system
-    '''
+    """
     return pcap.findalldevs()
 
 
 def config_loader(config_name):
-    '''Loads a .json configuration file
+    """Loads a .json configuration file
 
     The config name has to be relative to the helpers.py location
 
     :param config_name: The path name of the json configuration file
     :return: A dictionary of the configuration
-    '''
+    """
     with open(config_name, 'r') as f:
         config_json = f.read()
     return json.loads(config_json)
