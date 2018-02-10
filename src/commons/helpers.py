f6e7b08c42485f2f9f465c9202f8fbf4854ab59f
Thierry DECKER <mail@thierry-decker.com>
2018-02-10 07:05:50 +0100

    Added test fonction

--- src/commons/helpers.py
+++ src/commons/helpers.py
@@ -13,3 +13,11 @@ def get_adapters_names():
     :return: A list of the interfaces available on the system
     """
     return pcap.findalldevs()
+
+
+def main():
+    print(get_adapters_names())
+
+
+if __name__ == '__main__':
+    main()
