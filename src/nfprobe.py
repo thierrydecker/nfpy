07472da56e47355d7ccdc262d273ea52b5d5c52c
Thierry DECKER <mail@thierry-decker.com>
2018-02-11 20:03:29 +0100

    Updated nfprobe.py

--- src/nfprobe.py
+++ src/nfprobe.py
@@ -1,26 +1,4 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-
-"""This is the main module of the Netflow probe
-"""
-
-import argparse
-
-
-def main():
-    """Main function
-
-    This is the entry point of the application
-    :return:
-    """
-    parser = argparse.ArgumentParser()
-    group = parser.add_mutually_exclusive_group()
-    group.add_argument("-j", "--json", type=str, help="json configuration file name")
-    group.add_argument("-y", "--yaml", type=str, help="yaml configuration file name")
-    flags = parser.parse_args()
-    print(flags)
-
+from src.nfprobe.main import main
 
 if __name__ == '__main__':
     main()
