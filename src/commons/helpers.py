c7780b83e1aa66a32f9260c4111e99970af19450
Thierry DECKER <mail@thierry-decker.com>
2018-02-12 17:10:44 +0100

    Updated helpers.py

--- src/commons/helpers.py
+++ src/commons/helpers.py
@@ -15,17 +15,6 @@ def get_adapters_names():
     return pcap.findalldevs()
 
 
-def config_loader_json(config_name):
-    """Loads a .json configuration file
-
-    :param config_name: The path name of the json configuration file
-    :return: A dictionary of the configuration
-    """
-    with open(config_name, 'r') as f:
-        config_json = f.read()
-    return json.loads(config_json)
-
-
 def config_loader_yaml(config_name):
     """Loads a .yml configuration file
 
@@ -35,3 +24,16 @@ def config_loader_yaml(config_name):
     with open(config_name, 'r') as f:
         config_yml = f.read()
     return yaml.load(config_yml)
+
+
+def log_message(queue, level, module, function, message):
+    """Sends a message to a log worker process
+
+    :param queue: A queue to send the message to
+    :param level: A string identifying the level of the message (Either DEBUG, INFO, WARNING, ERROR, CRITICAL
+    :param module: A string identifying the source module of the message
+    :param function: A string identifying the source function module of the message
+    :param message: A string representing the message
+    :return:
+    """
+    queue.put((level, module, function, message))
